8a8e5f726d1e2b21b8b8f42588489c763262551b
KongaraNithin <nithinkongara1987@gmail.com>
2024-04-26 16:31:25 +0530

    Initial commit

---
+++ calapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+def input(request):
+    return render(request, 'input.html')
+def calculate(request):
+    x=int(request.POST["t1"])
+    y=int(request.POST["t2"])
+    op=request.POST["opt"]
+    con_dict={}
+    if op=="add":
+        con_dict["res"]=x+y
+    elif op=="sub":
+        con_dict["res"]=x-y
+    elif op=="mul":
+        con_dict["res"]=x*y
+    else:
+        con_dict["res"]=x/y
+    return render(request,'result.html',context=con_dict)
